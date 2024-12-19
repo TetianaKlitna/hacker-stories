d84fbf20256909b83b0b7308a0b6fee4cea9afb8
Tetiana Klitna <klitna.tetiana@gmail.com>
2024-12-18 23:01:02 -0800

    Asynchronous Data and Conditional Rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,103 @@
 //components
 import InputWithLabel from "./components/InputWithLabel";
 import List from "./components/List";
-import useStorageState from "./hooks/useStorageState"
-import { Fragment } from "react";
+import useStorageState from "./hooks/useStorageState";
+import { Fragment, useEffect, useState } from "react";
 
 const title = "React";
 
 const initialArticles = [
-  {objectId:0, title:"React", url:"https://reactjs.org/", author:"Jordan Walke", numComments:3, points:4},
-  {objectId:1, title:"Redux", url:"https://redux.js.org/", author:"Dan Abramov, Andrew Clark", numComments:2, points:5},
-  {objectId:2, title:"React", url:"https://reactjs.org/", author:"Jordan Walke", numComments:3, points:4},
-  {objectId:3, title:"Redux", url:"https://redux.js.org/", author:"Dan Abramov, Andrew Clark", numComments:2, points:5},
+  {
+    objectId: 0,
+    title: "React",
+    url: "https://reactjs.org/",
+    author: "Jordan Walke",
+    numComments: 3,
+    points: 4,
+  },
+  {
+    objectId: 1,
+    title: "Redux",
+    url: "https://redux.js.org/",
+    author: "Dan Abramov, Andrew Clark",
+    numComments: 2,
+    points: 5,
+  },
+  {
+    objectId: 2,
+    title: "React",
+    url: "https://reactjs.org/",
+    author: "Jordan Walke",
+    numComments: 3,
+    points: 4,
+  },
+  {
+    objectId: 3,
+    title: "Redux",
+    url: "https://redux.js.org/",
+    author: "Dan Abramov, Andrew Clark",
+    numComments: 2,
+    points: 5,
+  },
 ];
 
+const getAsyncArticles = () =>
+  new Promise((resolve) =>
+    setTimeout(() => resolve({ data: { stories: initialArticles } }), 2000)
+  );
 
 function App() {
-  
-  const [articlesList, setArticlesList] = useStorageState('articles', initialArticles);
-  const [searchTerm, setSearchTerm] = useStorageState('search', 'React');
+  const [articlesList, setArticlesList] = useStorageState("articles", []);
+  //const [articlesList, setArticlesList] = useState([]);
+  const [searchTerm, setSearchTerm] = useStorageState("search", "");
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    getAsyncArticles()
+      .then((result) => {
+        setArticlesList(result.data.stories);
+        setIsLoading(false);
+      })
+      .catch(() => setIsError(true));
+  }, []);
 
   const handleSearch = (event) => {
     const val = event.target.value;
-    setSearchTerm(val); 
-  }
+    setSearchTerm(val);
+  };
 
   const handleRemove = (id) => {
     const newList = articlesList.filter((item) => item.objectId !== id);
     setArticlesList(newList);
-  }
+  };
 
-  const filteredArticles = articlesList.filter(article => article.title.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1);
+  const filteredArticles = articlesList.filter(
+    (article) =>
+      article.title.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1
+  );
 
   return (
     <Fragment>
       <h1>Hello, {title} !</h1>
-      <InputWithLabel id = "search" value = {searchTerm} isFocused onInputChange = {handleSearch} >
-      <strong>Search:</strong>
+      <InputWithLabel
+        id="search"
+        value={searchTerm}
+        isFocused
+        onInputChange={handleSearch}
+      >
+        <strong>Search:</strong>
       </InputWithLabel>
       <hr />
-      <List list = {filteredArticles} onRemove = {handleRemove}/>
+      {isError && <p>Something go wrong...</p>}
+      {isLoading ? (
+        <p>
+          <strong>Loading...</strong>
+        </p>
+      ) : (
+        <List list={filteredArticles} onRemove={handleRemove} />
+      )}
     </Fragment>
   );
 }
